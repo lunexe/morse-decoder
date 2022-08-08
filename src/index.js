cf8397b0a1d11965cea54c2170def2efe76f8445
Egor Khamitsevich <lunexe@gmail.com>
2022-08-08 12:14:23 +0300

    feat: result

--- src/index.js
+++ src/index.js
@@ -38,9 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    if (expr.length$10) {
+        alert('error');
+    } else {
+        let string = '';
+        for (let i = 0; i <= expr.length - 10;) {
+            let letter = expr.substr(i, 10);
+            string = string + decodeSymbl(letter);
+            i = i + 10;
+        }
+        return string;
+    }
+
+}
+
+function decodeSymbl(expr) {
+    let dot = '.';
+    let dash = '-';
+    let result = ''; 
+
+    for (let i = 0; i < 10;) {
+        let exstr = expr.substr(i, 2);
+        if (exstr === '10') {
+            result = result + '.';
+            i = i + 2;
+        } else if (exstr === '11') {
+            result = result + '-';
+            i = i + 2;
+        } else if (exstr === '**') {
+            return ' ';
+        } else {
+            i += 2;
+        }
+    }
+    return MORSE_TABLE[result];
 }
 
 module.exports = {
     decode
-}
+}   
